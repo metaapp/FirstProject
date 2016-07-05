7e288f541d1ef2c21d111718991f86fccc940623
Kubo <mazgut@gmail.com>
2016-07-05 23:10:28 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,9 @@
+print("This line will be printed.")
+
+import csv
+with open('c:\\Users\\Kubo\\Downloads\\20160108065157.csv') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=';', quotechar='"')
+    for row in csvreader:
+        print(', '.join(row))
+
+print("Koniec")
