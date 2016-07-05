9db42c783ab22365f13dd15698de06c5a1480918
Kubo <mazgut@gmail.com>
2016-07-05 23:27:29 +0200

    Pridanie regular expression

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 print("This line will be printed.")
 
 import csv
+import re
 with open('c:\\Users\\Kubo\\Downloads\\20160108065157.csv') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=';', quotechar='"')
     for row in csvreader:
-        print(', '.join(row))
+        m = re.findall('(\d+)', row[0])
+        print(m)
 
 print("Skutocny koniec")
